cb6c10a635e5d058779c900c9c33f1f263bc4a26
Origamiii <16846485+origamiii@user.noreply.gitee.com>
2026-03-21 18:52:25 +0800

    单元格及其管理器，LevelData

---
+++ Assets/Scripts/Grid/CellType.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+  public enum CellType
+    {
+        Empty,
+        Wall,
+        PowerSource,
+        Wire,
+        HopeLamp,
+        ActivatablePower,
+        SignalAmplifier,
+        SignalMerger,
+        PhaseBlock,
+        PhaseTrigger,
+        CrossConnector,
+        PressSource
+    }
+
+
